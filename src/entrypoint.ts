b534e0661853658a59d56c405fc81a3324c80354
FujiHaruka <fizzyjohn1427@gmail.com>
2023-02-19 14:23:06 +0900

    not regularized beta function

--- src/entrypoint.ts
+++ src/entrypoint.ts
@@ -1,14 +1,11 @@
 import { Chart } from "https://esm.sh/chart.js@4.2.1/auto";
+import { createBeta } from "./func.ts";
 
-const data = [
-  { year: 2010, count: 10 },
-  { year: 2011, count: 20 },
-  { year: 2012, count: 15 },
-  { year: 2013, count: 25 },
-  { year: 2014, count: 22 },
-  { year: 2015, count: 30 },
-  { year: 2016, count: 28 },
-];
+const beta = createBeta(2, 3);
+const data = Array.from({ length: 100 }).map((_, i) => {
+  const x = i / 100;
+  return { x, y: beta(x) };
+});
 
 globalThis.addEventListener("DOMContentLoaded", () => {
   const canvas = document.getElementById("chart");
@@ -19,13 +16,15 @@ globalThis.addEventListener("DOMContentLoaded", () => {
   new Chart(
     canvas as HTMLCanvasElement,
     {
-      type: "bar",
+      type: "line",
       data: {
-        labels: data.map((row) => row.year),
+        labels: data.map((row) => row.x),
         datasets: [
           {
-            label: "Acquisitions by year",
-            data: data.map((row) => row.count),
+            label: "Beta(2, 3)",
+            data: data.map((row) => row.y),
+            pointRadius: 0,
+            fill: true,
           },
         ],
       },
