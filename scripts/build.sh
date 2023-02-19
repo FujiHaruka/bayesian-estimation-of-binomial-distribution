f2cd32cd7a2ffaf6f1dc6cc0252f6319f8fbf7ed
FujiHaruka <fizzyjohn1427@gmail.com>
2023-02-19 17:54:11 +0900

    Build with parcel

--- scripts/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -eux
-
-deno bundle --config tsconfig.json src/entrypoint.ts public/build.js
--- src/entrypoint.ts
+++ src/entrypoint.ts
@@ -1,12 +1,12 @@
-import { Chart } from "https://esm.sh/chart.js@4.2.1/auto";
-import jStat from "https://esm.sh/jstat@1.9.6"
+import { Chart } from "chart.js/auto";
+import jStat from "jstat";
 
 const data = Array.from({ length: 1000 }).map((_, i) => {
   const x = i / 1000;
   return { x, y: jStat.beta.pdf(x, 120, 80) };
 });
 
-console.log(jStat.beta.mode(120, 80))
+console.log(jStat.beta.mode(120, 80));
 
 globalThis.addEventListener("DOMContentLoaded", () => {
   const canvas = document.getElementById("chart");
@@ -14,22 +14,19 @@ globalThis.addEventListener("DOMContentLoaded", () => {
     throw new Error("Canvas not found");
   }
 
-  new Chart(
-    canvas as HTMLCanvasElement,
-    {
-      type: "line",
-      data: {
-        labels: data.map((row) => row.x),
-        datasets: [
-          {
-            label: "Beta(2, 3)",
-            data: data.map((row) => row.y),
-            pointRadius: 0,
-            fill: true,
-            cubicInterpolationMode: "monotone",
-          },
-        ],
-      },
+  new Chart(canvas as HTMLCanvasElement, {
+    type: "line",
+    data: {
+      labels: data.map((row) => row.x),
+      datasets: [
+        {
+          label: "Beta(2, 3)",
+          data: data.map((row) => row.y),
+          pointRadius: 0,
+          fill: true,
+          cubicInterpolationMode: "monotone",
+        },
+      ],
     },
-  );
+  });
 });
