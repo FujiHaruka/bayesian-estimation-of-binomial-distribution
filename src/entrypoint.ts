44460e2c597ca3c1f4b09f18926dfd795efbddf5
FujiHaruka <fizzyjohn1427@gmail.com>
2023-02-19 20:00:33 +0900

    Update the parameters of beta distribution dynamically

--- src/entrypoint.ts
+++ src/entrypoint.ts
@@ -1,26 +1,38 @@
 import { Chart } from "chart.js/auto";
 import jStat from "jstat";
 
-const data = Array.from({ length: 1000 }).map((_, i) => {
-  const x = i / 1000;
-  return { x, y: jStat.beta.pdf(x, 120, 80) };
-});
+const DATA_SIZE = 1000;
+
+function generateData(alpha: number, beta: number): { x: number; y: number }[] {
+  const data = Array.from({ length: DATA_SIZE }).map((_, i) => {
+    const x = i / DATA_SIZE;
+    return { x, y: jStat.beta.pdf(x, alpha, beta) };
+  });
+  return data;
+}
 
 console.log(jStat.beta.mode(120, 80));
 
-globalThis.addEventListener("DOMContentLoaded", () => {
-  const canvas = document.getElementById("chart");
-  if (!canvas) {
-    throw new Error("Canvas not found");
+window.addEventListener("DOMContentLoaded", () => {
+  const [chartCanvas, runButton] = ["#chart", "#run-simulation"].map(
+    (selector) => document.querySelector(selector)
+  );
+
+  if (!chartCanvas) {
+    throw new Error("Expected elements not found");
   }
 
-  new Chart(canvas as HTMLCanvasElement, {
+  let alpha = 2;
+  let beta = 3;
+
+  const data = generateData(alpha, beta);
+  const chart = new Chart(chartCanvas as HTMLCanvasElement, {
     type: "line",
     data: {
       labels: data.map((row) => row.x),
       datasets: [
         {
-          label: "Beta(2, 3)",
+          label: `Beta(${alpha}, ${beta})`,
           data: data.map((row) => row.y),
           pointRadius: 0,
           fill: true,
@@ -28,5 +40,15 @@ globalThis.addEventListener("DOMContentLoaded", () => {
         },
       ],
     },
+  });
+
+  (runButton as HTMLButtonElement).addEventListener("click", () => {
+    alpha += 1;
+    beta += 1;
+
+    chart.data.datasets[0].label = `Beta(${alpha}, ${beta})`;
+    chart.data.datasets[0].data = generateData(alpha, beta).map(({ y }) => y);
+
+    chart.update();
   });
 });
--- src/func.ts
+++
@@ -1,5 +0,0 @@
-export function createBeta(a: number, b: number) {
-  return function beta(x: number) {
-    return Math.pow(x, a - 1) * Math.pow(1 - x, b - 1);
-  };
-}
